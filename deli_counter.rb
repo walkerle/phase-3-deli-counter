aad6087009ffa7c0a2ca036de626571d306e6216
Walker Le <walker.le88@gmail.com>
2023-02-01 13:05:55 -0700

    commit tree 61356dfb559c55aa21b022c7b1cdeb9fa9de43ce

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line deli
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number deli, name
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving deli
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli[0]}."
+    # puts "Currently serving #{deli.first}."
+    deli.delete_at(0)
+    # deli.shift
+    # deli.delete(deli.first)
+  end
+end
+
+# binding.pry
+# 0
